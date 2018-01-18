fb61604e6447b20990b4516354978c865bb83cae
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-01-18 16:20:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ function deleteFromObjectByKey(object, key) {
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  var newObj = 
+  var newObj = object
   delete newObj.key
-  return newObj
 }

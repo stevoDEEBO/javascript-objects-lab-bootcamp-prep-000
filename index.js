d203754357141f625d3a2bf92ee5e4d3604ca35c
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-01-18 15:52:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  var obj = object
-  obj.key = value
-  return obj
+  var object = recipes
+  object.key = value
+  return object
 }

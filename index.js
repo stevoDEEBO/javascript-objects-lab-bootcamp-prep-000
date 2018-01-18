238180f057a554f5338cb21f12a86706e8881082
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-01-18 16:25:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var recipes = {
 }
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  var obj = Object.assign({}, recipes, { [key]: value })
+  var obj = Object.assign({}, object, { [key]: value })
   recipes.prop2 = 2
   return obj
 }
